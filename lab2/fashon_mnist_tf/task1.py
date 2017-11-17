5d2d266ff573e898855b1684a1f19de3b199754f
konsoz <sozinov@kth.se>
2017-11-17 12:00:39 +0100

    lab2 init

---
+++ lab2/fashon_mnist_tf/task1.py
@@ -0,0 +1,113 @@
+# all tensorflow api is accessible through this
+import tensorflow as tf
+# to visualize the resutls
+import matplotlib.pyplot as plt
+# 70k mnist dataset that comes with the tensorflow container
+from tensorflow.examples.tutorials.mnist import input_data
+
+tf.set_random_seed(0)
+
+IMAGE_SIZE = 28
+IMAGE_PIXELS = IMAGE_SIZE * IMAGE_SIZE
+NUM_CLASSES = 10
+
+LEARNING_RATE = 0.5
+# load data
+mnist = input_data.read_data_sets('data/fashion', one_hot=True)
+
+X = tf.placeholder(tf.float32, [None, IMAGE_SIZE, IMAGE_SIZE, 1])
+y_ = tf.placeholder(tf.float32, [None, NUM_CLASSES]) # correct answers(labels)
+W = tf.Variable(tf.zeros([IMAGE_PIXELS, NUM_CLASSES])) # weights W[784, 10] 784=28*28
+b = tf.Variable(tf.zeros([NUM_CLASSES])) # biases b[10]
+XX = tf.reshape(X, [-1, IMAGE_PIXELS]) # flatten the images into a single line of pixels
+
+# 2. Define the model - compute predicitions
+y = tf.nn.softmax(tf.matmul(XX, W) + b)
+# 3. Define the loss function
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+# 4. Define the accuracy
+correct_predictions = tf.equal(tf.arg_max(y, 1), tf.arg_max(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+# 5. Train with an Optimizer
+train_step = tf.train.GradientDescentOptimizer(LEARNING_RATE).minimize(cross_entropy)
+
+# initialize
+init = tf.initialize_all_variables()
+sess = tf.Session()
+sess.run(init)
+
+
+def training_step(i, update_test_data, update_train_data):
+
+    print "\r", i,
+    ####### actual learning 
+    # reading batches of 100 images with 100 labels
+    batch_X, batch_Y = mnist.train.next_batch(100)
+    # the backpropagation training step
+    sess.run(train_step, feed_dict={XX: batch_X, y_: batch_Y})
+
+    ####### evaluating model performance for printing purposes
+    # evaluation used to later visualize how well you did at a particular time in the training
+    train_a = []
+    train_c = []
+    test_a = []
+    test_c = []
+    if update_train_data:
+        a, c = sess.run([accuracy, cross_entropy], feed_dict={XX: batch_X, y_: batch_Y})
+        train_a.append(a)
+        train_c.append(c)
+
+    if update_test_data:
+        a, c = sess.run([accuracy, cross_entropy], feed_dict={XX: mnist.test.images, y_: mnist.test.labels})
+        test_a.append(a)
+        test_c.append(c)
+
+
+    return (train_a, train_c, test_a, test_c)
+
+
+# 6. Train and test the model, store the accuracy and loss per iteration
+
+train_a = []
+train_c = []
+test_a = []
+test_c = []
+
+training_iter = 10000
+epoch_size = 100
+for i in range(training_iter):
+    test = False
+    if i % epoch_size == 0:
+        test = True
+    a, c, ta, tc = training_step(i, test, test)
+    train_a += a
+    train_c += c
+    test_a += ta
+    test_c += tc
+
+# 7. Plot and visualise the accuracy and loss
+
+# accuracy training vs testing dataset
+plt.plot(train_a)
+plt.plot(test_a)
+plt.grid(True)
+plt.show()
+
+# loss training vs testing dataset
+plt.plot(train_c)
+plt.plot(test_c)
+plt.grid(True)
+plt.show()
+
+# Zoom in on the tail of the plots
+zoom_point = 50
+x_range = range(zoom_point,training_iter/epoch_size)
+plt.plot(x_range, train_a[zoom_point:])
+plt.plot(x_range, test_a[zoom_point:])
+plt.grid(True)
+plt.show()
+
+plt.plot(train_c[zoom_point:])
+plt.plot(test_c[zoom_point:])
+plt.grid(True)
+plt.show()
