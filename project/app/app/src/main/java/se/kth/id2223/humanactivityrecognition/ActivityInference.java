5c8fd248f3725877e6bdf49e7ee8709e1244d7c3
konsoz <sozinov@kth.se>
2017-12-16 14:51:15 +0100

    app for local training

--- project/app/app/src/main/java/se/kth/id2223/humanactivityrecognition/ActivityInference.java
+++ project/app/app/src/main/java/se/kth/id2223/humanactivityrecognition/ActivityInference.java
@@ -3,9 +3,6 @@ package se.kth.id2223.humanactivityrecognition;
 import android.content.Context;
 import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Base class used for predicting using TensorFlow java library
  */
@@ -16,41 +13,28 @@ public class ActivityInference {
   }
 
   private TensorFlowInferenceInterface mInferenceInterface;
-  private final String mPathToModel;
-  private final String mInputNodeName;
-  private final String [] mOutputNodes;
-  private final String mOutputNodeName;
-  private final long[] mInputSize;
-  private final int mOutputSize;
-  private final List<Integer> mPredictions;
-  private final static String sPathToModel = "file:///android_asset/model_name.pb";
 
+  private final static String PATH_TO_MODEL = "file:///android_asset/frozen_har.pb";
   // Should be defined in training script
   // Input node name, i.e. placeholder's name for input of the data
-  private final static String sInputNodeName = "input";
+  private final static String INPUT_NODE_NAME = "input";
   // Output node name, the one that computes softmax activation on logits
-  private final static String sOutPutNodeName = "output";
+  private final static String OUTPUT_NODE_NAME = "y_";
   // Input size, num_examples x num_features
-  private final static long[] sInputSize = {1, 32};
+  private final static long[] INPUT_SIZE = {1, 100, 3};
+  private static final String[] OUTPUT_NODES = {"y_"};
   // Output size, number of target labels
-  private final static int sOutputSize = 2;
+  private final static int NUM_OUTPUT_CLASSSES = 7;
 
-  public ActivityInference(String inputNodeName, String outputNodeName, long[] inputSize, int outputSize, String pathToModel, final Context context){
-    mPredictions = new ArrayList<>();
-    mPathToModel = pathToModel;
-    mInputNodeName = inputNodeName;
-    mOutputNodes = new String[]{outputNodeName};
-    mOutputNodeName = outputNodeName;
-    mInputSize = inputSize;
-    mOutputSize = outputSize;
-    mInferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), mPathToModel);
+  public ActivityInference(final Context context){
+    mInferenceInterface = new TensorFlowInferenceInterface(context.getAssets(), PATH_TO_MODEL);
   }
 
   public float[] getActivityProbabilities(float [] inputSignal){
-    float[] result = new float[mOutputSize];
-    mInferenceInterface.feed(mInputNodeName,inputSignal,mInputSize);
-    mInferenceInterface.run(mOutputNodes);
-    mInferenceInterface.fetch(mOutputNodeName,result);
+    float[] result = new float[NUM_OUTPUT_CLASSSES];
+    mInferenceInterface.feed(INPUT_NODE_NAME,inputSignal,INPUT_SIZE);
+    mInferenceInterface.run(OUTPUT_NODES);
+    mInferenceInterface.fetch(OUTPUT_NODE_NAME,result);
     return result;
   }
 }
--- project/app/app/src/main/java/se/kth/id2223/humanactivityrecognition/MainActivity.java
+++ project/app/app/src/main/java/se/kth/id2223/humanactivityrecognition/MainActivity.java
@@ -1,13 +1,148 @@
 package se.kth.id2223.humanactivityrecognition;
 
-import android.support.v7.app.AppCompatActivity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener, TextToSpeech.OnInitListener {
+
+  private static final int N_SAMPLES = 100;
+  private static List<Float> x;
+  private static List<Float> y;
+  private static List<Float> z;
+  private TextView bikingTextView;
+
+  private TextView stairuUPTextView;
+  private TextView sittingTextView;
+  private TextView standingTextView;
+  private TextView stairDownTextView;
+  private TextView walkingTextView;
+  private TextToSpeech textToSpeech;
+  private float[] results;
+  private ActivityInference classifier;
+
+  private String[] labels = {"Sitting", "Standing", "Stair up", "Walking", "Biking", "Stair down"};
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    x = new ArrayList<>();
+    y = new ArrayList<>();
+    z = new ArrayList<>();
+
+    bikingTextView = (TextView) findViewById(R.id.biking_prob);
+    sittingTextView = (TextView) findViewById(R.id.sitting_prob);
+    standingTextView = (TextView) findViewById(R.id.standing_prob);
+    walkingTextView = (TextView) findViewById(R.id.walking_prob);
+    stairuUPTextView = (TextView) findViewById(R.id.stair_up_prob);
+    stairDownTextView = (TextView) findViewById(R.id.stair_down_prob);
+
+    classifier = new ActivityInference(getApplicationContext());
+
+    textToSpeech = new TextToSpeech(this, this);
+    textToSpeech.setLanguage(Locale.US);
   }
+
+  @Override
+  public void onInit(int status) {
+    Timer timer = new Timer();
+    timer.scheduleAtFixedRate(new TimerTask() {
+      @Override
+      public void run() {
+        if (results == null || results.length == 0) {
+          return;
+        }
+        float max = -1;
+        int idx = -1;
+        for (int i = 0; i < results.length; i++) {
+          if (results[i] > max) {
+            idx = i;
+            max = results[i];
+          }
+        }
+
+        textToSpeech.speak(labels[idx], TextToSpeech.QUEUE_ADD, null, Integer.toString(new Random().nextInt()));
+      }
+    }, 2000, 5000);
+  }
+
+  protected void onPause() {
+    getSensorManager().unregisterListener(this);
+    super.onPause();
+  }
+
+  protected void onResume() {
+    super.onResume();
+    getSensorManager().registerListener(this, getSensorManager().getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+    activityPrediction();
+    x.add(event.values[0]);
+    y.add(event.values[1]);
+    z.add(event.values[2]);
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int i) {
+
+  }
+
+  private void activityPrediction() {
+    if (x.size() == N_SAMPLES && y.size() == N_SAMPLES && z.size() == N_SAMPLES) {
+      List<Float> data = new ArrayList<>();
+      data.addAll(x);
+      data.addAll(y);
+      data.addAll(z);
+
+      results = classifier.getActivityProbabilities(toFloatArray(data));
+
+      bikingTextView.setText(Float.toString(round(results[0], 2)));
+      stairuUPTextView.setText(Float.toString(round(results[1], 2)));
+      sittingTextView.setText(Float.toString(round(results[2], 2)));
+      standingTextView.setText(Float.toString(round(results[3], 2)));
+      stairDownTextView.setText(Float.toString(round(results[4], 2)));
+      walkingTextView.setText(Float.toString(round(results[5], 2)));
+
+      x.clear();
+      y.clear();
+      z.clear();
+    }
+  }
+
+  private float[] toFloatArray(List<Float> list) {
+    int i = 0;
+    float[] array = new float[list.size()];
+
+    for (Float f : list) {
+      array[i++] = (f != null ? f : Float.NaN);
+    }
+    return array;
+  }
+
+  private static float round(float d, int decimalPlace) {
+    BigDecimal bd = new BigDecimal(Float.toString(d));
+    bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+    return bd.floatValue();
+  }
+
+  private SensorManager getSensorManager() {
+    return (SensorManager) getSystemService(SENSOR_SERVICE);
+  }
+
 }
